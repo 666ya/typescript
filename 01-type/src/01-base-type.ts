2b875e24ce795e6ac46713834c22d6291fcf0352
焦文儒 <zzd>
2022-01-19 13:40:05 +0800

    feat: primative type

---
+++ 01-type/src/01-base-type.ts
@@ -0,0 +1,12 @@
+const baseStr: string = 'str'
+
+const baseNumber: number = 100
+const baseNumberNan: number = NaN
+
+const baseBoolean: boolean = true
+
+const baseUndefine: undefined = undefined
+
+const baseNull: null = null
+
+const baseSysmbol: symbol = Symbol('symbol')
---
+++ 01-type/src/02-array-type.ts
@@ -0,0 +1,3 @@
+const baseArray: number[] = [1, 3]
+
+const baseArray1: Array<number> = [1, 4]
