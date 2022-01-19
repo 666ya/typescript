194361a5cd4c2a9d8c7df855328f28b2deb19741
焦文儒 <zzd>
2022-01-19 14:18:55 +0800

    fix: 删除编译文件

--- dist/02-array-type.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const baseArray = [1, 3];
-const baseArray1 = [1, 4];
--- dist/12-enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Direction;
-(function (Direction) {
-    Direction[Direction["Up"] = 1] = "Up";
-    Direction[Direction["Down"] = 2] = "Down";
-    Direction[Direction["Left"] = 3] = "Left";
-    Direction[Direction["Right"] = 4] = "Right";
-})(Direction || (Direction = {}));
-console.log(Direction.Down);
